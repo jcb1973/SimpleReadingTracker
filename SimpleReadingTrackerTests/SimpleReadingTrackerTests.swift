c2c2f51f860bfb1b1810075cf3d102d6895e1ecf
jcb <jcb1973@gmail.com>
2026-02-12 08:34:50 +0100

    Initial Commit

---
+++ SimpleReadingTrackerTests/SimpleReadingTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleReadingTrackerTests.swift
+//  SimpleReadingTrackerTests
+//
+//  Created by John Cieslik-Bridgen on 2026-02-12.
+//
+
+import Testing
+@testable import SimpleReadingTracker
+
+struct SimpleReadingTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
